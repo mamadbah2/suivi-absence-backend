697837cc0faf9aae1d19ed9838b152c98bb0a92f
annandiaye11 <anna.ndiaye1@ism.edu.sn>
2025-05-30 18:10:22 +0000

    hhh

--- src/main/java/sn/dev/suiviabsence/mobile/dto/requests/UploadJustificationRequestDto.java
+++ src/main/java/sn/dev/suiviabsence/mobile/dto/requests/UploadJustificationRequestDto.java
@@ -1,8 +1,11 @@
-package com.suiviabsence.mobile.dto.request;
+package sn.dev.suiviabsence.mobile.dto.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class UploadJustificationRequest {
     private MultipartFile fichier;
